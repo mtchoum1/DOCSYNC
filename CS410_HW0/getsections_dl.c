231978b1db98aa0214f1710f440d8746b43a6ad7
Moryan Tchoumi Noukoua <mtchoum1@csa2.bu.edu>
2024-02-12 17:32:41 -0500

    test

--- CS410_HW0/getsections_dl.c
+++ CS410_HW0/getsections_dl.c
@@ -1,17 +1,17 @@
 #include "libobjdata.h"
 #include <dlfcn.h>
-void* lib_handle;
 
 
 int main(int argc, char *argv[])
 {
+  void* lib_handle;
   lib_handle = dlopen("libobjdata.so", RTLD_LAZY);
   if (!lib_handle)
   {
     dlerror();
     return 1;
   }
-  struct local_file* (*printsect);
+  struct local_file *(*printsect)(bfd *abfd, asection *sect, void *obj);
   const char* error_msg;
   struct local_file* a_file;
   printsect = dlsym(lib_handle, "print_section_info");
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     bfd_perror("Unrecognized file format");
     return 1;
   }
-  bfd_map_over_sections(abfd, printsect, NULL);
+  bfd_map_over_sections(abfd, printsect(abfd, section, NULL), NULL);
   bfd_close(abfd);
   dlclose(lib_handle);
   return 0;

b82509c5a1c3a2a349ee4d8d36328c1737893bfe
Moryan Tchoumi Noukoua <mtchoum1@csa2.bu.edu>
2024-02-12 20:56:14 -0500

    Got getsections_dl link to the shared library

--- CS410_HW0/getsections_dl.c
+++ CS410_HW0/getsections_dl.c
@@ -3,28 +3,48 @@
 
 int main(int argc, char *argv[])
 {
-  void* lib_handle;       
+  void* lib_handle;
+  bfd *abfd;
 
-  lib_handle = dlopen("libobjdata.so", RTLD_LAZY);
+  lib_handle = dlopen("./libobjdata.so", RTLD_LAZY);
+  if (!lib_handle) {
+    dlerror();
+    return 1;
+  }
+
   struct local_file* (*printsect)(bfd *abfd, asection *section, void *obj);
   const char* error_msg;
   printsect = dlsym(lib_handle, "print_section_info");
   error_msg = dlerror();
-  bfd *abfd;
+  if (error_msg) {
+    dlerror();
+    dlclose(lib_handle);
+    return 1;
+  }
+
   bfd_init();
   if (argc != 2)
   {
-    bfd_perror("Error No Argument");
+    dlclose(lib_handle);
     return 1;
   }
 
   abfd = bfd_openr(argv[1], NULL);
-  if (!bfd_check_format(abfd, bfd_object))
-  {
-    bfd_perror("Unrecognized file format");
+  if (!abfd) {
+    bfd_errmsg(bfd_get_error());
+    dlclose(lib_handle);
     return 1;
   }
-  bfd_map_over_sections(abfd, (printsect), NULL);
+
+  if (!bfd_check_format(abfd, bfd_object))
+  {
+    bfd_errmsg(bfd_get_error());
+    bfd_close(abfd);
+    dlclose(lib_handle);
+    return 1;
+  }
+
+  bfd_map_over_sections(abfd, (void (*)(bfd *, asection *, void *))printsect, NULL);
   bfd_close(abfd);
   dlclose(lib_handle);
   return 0;

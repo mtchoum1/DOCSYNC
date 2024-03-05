7e1773742bd4c46c0b233b281412389157d5735f
Moryan Tchoumi Noukoua <mtchoum1@csa2.bu.edu>
2024-03-04 23:53:51 -0500

    fix ? match case

---
+++ HW1/my_printf.c
@@ -0,0 +1,94 @@
+#include "libobjdata.h"
+
+void itoa (char *buf, int base, int d)
+{
+  char *p = buf;
+  char *p1, *p2;
+  unsigned long ud = d;
+  int divisor = 10;
+     
+  /* If %d is specified and D is minus, put `-' in the head. */
+  if (base == 'd' && d < 0)
+    {
+      *p++ = '-';
+      buf++;
+      ud = -d;
+    }
+  else if (base == 'x')
+    divisor = 16;
+     
+  /* Divide UD by DIVISOR until UD == 0. */
+  do
+    {
+      int remainder = ud % divisor;
+     
+      *p++ = (remainder < 10) ? remainder + '0' : remainder + 'a' - 10;
+    }
+  while (ud /= divisor);
+     
+  /* Terminate BUF. */
+  *p = 0;
+     
+  /* Reverse BUF. */
+  p1 = buf;
+  p2 = p - 1;
+  while (p1 < p2)
+    {
+      char tmp = *p1;
+      *p1 = *p2;
+      *p2 = tmp;
+      p1++;
+      p2--;
+    }
+}
+
+void my_printf(const char *format, ...) {
+    // Pointer to the first argument after the format string
+    int *args = &format + 1;
+    char *temp = (char*)*args;
+    char buf[100];
+    itoa(buf, 's', temp);
+    printf("%s", temp);
+    // Iterate through the format string
+    // while (*format != '\0') 
+    // {
+    //     if (*format == '%') 
+    //     {
+    //         format++; // Move to the next character after '%'
+    //         switch (*format) 
+    //         {
+    //             case 's':
+    //                 printf("%s", (char*)*args);
+    //                 break;
+    //             case 'c':
+    //                 printf("%c", (char)*args);
+    //                 break;
+    //             case 'd':
+    //                 printf("%d", *args);
+    //                 break;
+    //             case 'u':
+    //                 printf("%u", (unsigned int)*args);
+    //                 break;
+    //             case 'x':
+    //                 printf("%x", *args);
+    //                 break;
+    //             default:
+    //                 putchar('%'); // Print '%' character if the format code is not recognized
+    //                 putchar(*format);
+    //                 break;
+    //         }
+    //         args++; // Move to the next argument
+    //     } 
+    //     else 
+    //     {
+    //         putchar(*format);
+    //     }
+
+    //     format++; // Move to the next character in the format string
+    // }
+}
+
+int main() {
+    my_printf("Hello, %s! Your age is %d and your score is %u. Hex value: %x\n", "John", 25, 100, 0xDEADBEEF);
+    return 0;
+}

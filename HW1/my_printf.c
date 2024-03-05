691a0a53d2df95c26141f6c2a9b9b5d8d4080595
mtchoum1 <123125223+mtchoum1@users.noreply.github.com>
2024-03-05 15:02:22 -0500

    Update my_printf.c

--- HW1/my_printf.c
+++ HW1/my_printf.c
@@ -42,45 +42,76 @@ void itoa (char *buf, int base, int d)
     }
 }
 
+void itoa (char *buf, int base, int d)
+{
+  char *p = buf;
+  char *p1, *p2;
+  unsigned long ud = d;
+  int divisor = 10;
+     
+  /* If %d is specified and D is minus, put `-' in the head. */
+  if (base == 'd' && d < 0)
+    {
+      *p++ = '-';
+      buf++;
+      ud = -d;
+    }
+  else if (base == 'x')
+    divisor = 16;
+     
+  /* Divide UD by DIVISOR until UD == 0. */
+  do
+    {
+      int remainder = ud % divisor;
+     
+      *p++ = (remainder < 10) ? remainder + '0' : remainder + 'a' - 10;
+    }
+  while (ud /= divisor);
+     
+  /* Terminate BUF. */
+  *p = 0;
+     
+  /* Reverse BUF. */
+  p1 = buf;
+  p2 = p - 1;
+  while (p1 < p2)
+    {
+      char tmp = *p1;
+      *p1 = *p2;
+      *p2 = tmp;
+      p1++;
+      p2--;
+    }
+}
+
 void my_printf(char *str, ...)
 {
   int *arg = &str + 4;
     
   while (*str != '\0')
   {
-    if (*str == '%' && *(str + 1) == 'd') 
+    if (*str == '%') 
     {
-      printf("%d", *arg);
-      arg += 2; 
-      str += 2; 
-    } 
-    else if (*str == '%' && *(str + 1) == 's') 
-    {
-      printf("%s", *arg);
-      arg += 2;
-      str += 2; 
-    } 
-    else if (*str == '%' && *(str + 1) == 'c') 
-    {
-      printf("%c", *arg);
-      arg += 2;
-      str += 2; 
-    } 
-    else if (*str == '%' && *(str + 1) == 'x') 
-    {
-      printf("%x", *arg);
-      arg += 2;
-      str += 2; 
-    } 
-    else if (*str == '%' && *(str + 1) == 'u') 
-    {
-      printf("%u", *arg);
-      arg += 2;
-      str += 2; 
-    } 
+        if (*str == '%' && *(str + 1) == 's') 
+        {
+          write(1, *arg, sizeof(*arg));
+          arg += 2;
+          str += 2; 
+        } 
+        else 
+        {
+            char buf[sizeof(*arg)];
+            itoa(buf, *(str + 1), *arg);
+            write(1, buf, strlen(buf));
+          arg += 2;
+          str += 2; 
+        } 
+    }
     else 
     {
-      putchar(*str);
+        char buf[sizeof(*str)]; 
+        buf[0] = *str;
+      write(1, buf, sizeof(*str));
       str++;
     }
   }
@@ -88,7 +119,7 @@ void my_printf(char *str, ...)
 
 int main()
 {
-  my_printf("Hello, %s! Your age is %d and your score is %u. Hex value: %x\n", "John", 25, 100, 0xDEADBEEF);
+  my_printf("Hello, %s! Your age is %d and your score is %u. Hex value: %x\n", "John", 25, 100);
   
   return 0;
 }

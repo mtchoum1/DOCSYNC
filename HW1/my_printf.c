f9d54b4eec3a5c9823ae67db62a9a5d71479c494
Moryan Tchoumi Noukoua <mtchoum1@csa2.bu.edu>
2024-03-07 10:12:13 -0500

    Complete my_printf

--- HW1/my_printf.c
+++ HW1/my_printf.c
@@ -44,99 +44,101 @@ void itoa (char *buf, int base, int d)
 
 void my_printf(char *str, ...)
 {
-    void *stack_address;
-    char *arg[5];
-    int i = 0;
-    __asm__ (".intel_syntax noprefix;"
-             "mov %0, rsi;"
-             "mov %1, rdx;"
-             "mov %2, rcx;"
-             "mov %3, r8;"
-             "mov %4, r9;"
-             "mov rax, rbp;"
-             "add rax, 16;"
-             "mov %5, rax;"
-             ".att_syntax prefix"
-             : "=r" (arg[0]), "=r" (arg[1]), "=r" (arg[2]), "=r" (arg[3]), "=r" (arg[4]), "=r"(stack_address)
-             :
-             : "esi", "edx", "ecx", "r8", "r9", "rax"
-            );
-    void **argp = stack_address;
-    while (*str != '\0')
+  void *stack_address;
+  char *arg[5];
+  int i = 0;
+  __asm__
+  (
+   ".intel_syntax noprefix;"
+   "mov %0, rsi;"
+   "mov %1, rdx;"
+   "mov %2, rcx;"
+   "mov %3, r8;"
+   "mov %4, r9;"
+   "mov rax, rbp;"
+   "add rax, 16;"
+   "mov %5, rax;"
+   ".att_syntax prefix"
+   : "=r" (arg[0]), "=r" (arg[1]), "=r" (arg[2]), "=r" (arg[3]), "=r" (arg[4]), "=r"(stack_address)
+   :
+   : "esi", "edx", "ecx", "r8", "r9", "rax"
+  );
+  void **argp = stack_address;
+  while (*str != '\0')
+  {
+    if (*str == '%')
     {
-        if (*str == '%')
-        {
-            if (i < 5)
-            {
-                if (*(str + 1) == 's')
-                {
-                    write(1, arg[i], strlen(arg[i]));
-                    i++;
-                    str += 2;
-                }
-                else if (*(str + 1) == 'c')
-                {
-                    char buf[1];
-                    buf[0] = arg[i];
-                    write(1, buf, 1);
-                    i++;
-                    str += 2;
-                }
-                else
-                {
-                    char buf[11];
-                    itoa(buf, *(str + 1), arg[i]);
-                    write(1, buf, strlen(buf));
-                    i++;
-                    str += 2;
-                }
-            }
-            else
-            {
-                if (*(str + 1) == 's')
-                {
-                    char *s = *(char**)(argp);
-                    write(1, s, strlen(s));
-                    argp++;
-                    str += 2;
-                }
-                else if (*(str + 1) == 'c')
-                {
-                    char c = *(char*)(argp);
-                    char buf[1];
-                    buf[0] = c;
-                    write(1, buf, 1);
-                    argp++;
-                    str += 2;
-                }
-                else if (*(str + 1) == 'd')
-                {
-                    int num = *(int*)(argp);
-                    char buf[11];
-                    itoa(buf, *(str + 1), arg[i]);
-                    write(1, buf, strlen(buf));
-                    argp++;
-                    str += 2;
-                }
-                else
-                {
-                    unsigned int num = *(unsigned int*)(argp);
-                    char buf[11];
-                    itoa(buf, *(str + 1), arg[i]);
-                    write(1, buf, strlen(buf));
-                    argp++;
-                    str += 2;
-                }
-            }
-        }
-        else
-        {
-            char buf[1];
-            buf[0] = *str;
-            write(1, buf, 1);
-            str++;
-        }
+      if (i < 5)
+      {
+	if (*(str + 1) == 's')
+	{
+	  write(1, arg[i], strlen(arg[i]));
+	  i++;
+	  str += 2;
+	}
+	else if (*(str + 1) == 'c')
+	{
+	  char buf[1];
+	  buf[0] = arg[i];
+	  write(1, buf, 1);
+	  i++;
+	  str += 2;
+	}
+	else
+	{
+	  char buf[11];
+	  itoa(buf, *(str + 1), arg[i]);
+	  write(1, buf, strlen(buf));
+	  i++;
+	  str += 2;
+	}
+      }
+      else
+      {
+	if (*(str + 1) == 's')
+	{
+	  char *s = *(char**)(argp);
+	  write(1, s, strlen(s));
+	  argp++;
+	  str += 2;
+	}
+	else if (*(str + 1) == 'c')
+	{
+	  char c = *(char*)(argp);
+	  char buf[1];
+	  buf[0] = c;
+	  write(1, buf, 1);
+	  argp++;
+	  str += 2;
+	}
+	else if (*(str + 1) == 'd')
+	{
+	  int num = *(int*)(argp);
+	  char buf[11];
+	  itoa(buf, *(str + 1), arg[i]);
+	  write(1, buf, strlen(buf));
+	  argp++;
+	  str += 2;
+	}
+	else
+	{
+	  unsigned int num = *(unsigned int*)(argp);
+	  char buf[11];
+	  itoa(buf, *(str + 1), arg[i]);
+	  write(1, buf, strlen(buf));
+	  argp++;
+	  str += 2;
+	}
+      }
     }
+    else
+    {
+      char buf[1];
+      buf[0] = *str;
+      write(1, buf, 1);
+      str++;
+    }
+  }
 }
 
 int main()

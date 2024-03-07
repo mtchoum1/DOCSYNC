c77f2413c7a0c05abe6a5508c96422a60c61ce1e
mtchoum1 <123125223+mtchoum1@users.noreply.github.com>
2024-03-07 09:56:15 -0500

    Update my_printf.c

--- HW1/my_printf.c
+++ HW1/my_printf.c
@@ -44,60 +44,102 @@ void itoa (char *buf, int base, int d)
 
 void my_printf(char *str, ...)
 {
+    void *stack_address;
     char *arg[5];
-    register int *s __asm__( "esi" );
-    arg[0] = s;
-    register int *ss __asm__( "edx" );
-    arg[1] = ss;
-    register int *sss __asm__( "ecx" );
-    arg[2] = sss;
-    register int *ssss __asm__( "r8" );
-    arg[3] = ssss;
-    register int *sssss __asm__( "r9" );
-    arg[4] = sssss;
     int i = 0;
-  
-    
-  while (*str != '\0')
-  {
-    if (*str == '%') 
+    __asm__ (".intel_syntax noprefix;"
+             "mov %0, rsi;"
+             "mov %1, rdx;"
+             "mov %2, rcx;"
+             "mov %3, r8;"
+             "mov %4, r9;"
+             "mov rax, rbp;"
+             "add rax, 16;"
+             "mov %5, rax;"
+             ".att_syntax prefix"
+             : "=r" (arg[0]), "=r" (arg[1]), "=r" (arg[2]), "=r" (arg[3]), "=r" (arg[4]), "=r"(stack_address)
+             :
+             : "esi", "edx", "ecx", "r8", "r9", "rax"
+            );
+    void **argp = stack_address;
+    while (*str != '\0')
     {
-        if (*(str + 1) == 's') 
+        if (*str == '%')
         {
-          write(1, arg[i], strlen(arg[i]));
-          i++;
-          str += 2; 
+            if (i < 5)
+            {
+                if (*(str + 1) == 's')
+                {
+                    write(1, arg[i], strlen(arg[i]));
+                    i++;
+                    str += 2;
+                }
+                else if (*(str + 1) == 'c')
+                {
+                    char buf[1];
+                    buf[0] = arg[i];
+                    write(1, buf, 1);
+                    i++;
+                    str += 2;
+                }
+                else
+                {
+                    char buf[11];
+                    itoa(buf, *(str + 1), arg[i]);
+                    write(1, buf, strlen(buf));
+                    i++;
+                    str += 2;
+                }
+            }
+            else
+            {
+                if (*(str + 1) == 's')
+                {
+                    char *s = *(char**)(argp);
+                    write(1, s, strlen(s));
+                    argp++;
+                    str += 2;
+                }
+                else if (*(str + 1) == 'c')
+                {
+                    char c = *(char*)(argp);
+                    write(1, c, 1);
+                    argp++;
+                    str += 2;
+                }
+                else if (*(str + 1) == 'd')
+                {
+                    int num = *(int*)(argp);
+                    char buf[11];
+                    itoa(buf, *(str + 1), arg[i]);
+                    write(1, buf, strlen(buf));
+                    argp++;
+                    str += 2;
+                }
+                else
+                {
+                    unsigned int num = *(unsigned int*)(argp);
+                    char buf[11];
+                    itoa(buf, *(str + 1), arg[i]);
+                    write(1, buf, strlen(buf));
+                    argp++;
+                    str += 2;
+                }
+            }
         }
-        else if (*(str + 1) == 'c') 
+        else
         {
-            char buf[1]; 
-            buf[0] = arg[i];
-          write(1, buf, 1);
-            i++;
-          str += 2; 
+            char buf[1];
+            buf[0] = *str;
+            write(1, buf, 1);
+            str++;
         }
-        else 
-        {
-	  char buf[sizeof(arg[i])];
-	  itoa(buf, *(str + 1), arg[i]);
-	  write(1, buf, strlen(buf));
-          i++;
-          str += 2; 
-        } 
     }
-    else 
-    {
-      char buf[sizeof(*str)]; 
-      buf[0] = *str;
-      write(1, buf, sizeof(*str));
-      str++;
-    }
-  }
 }
 
 int main()
 {
-  my_printf("Hello, %s! Your age is %d and your score is %u. Hex value: %x char: %c\n", "John", 25, 100, 0xDEADBEEF, 'h');
+  my_printf("Hello, %s! Your age is %d and your score is %u. Hex value: %x Char: %c String: %s WT%c \n", "John", 25, 100, 0xDEADBEEF, 'c', "hello", 'f');
   
   return 0;
 }
